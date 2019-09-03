946071ae1ec200d572a2f4bfd1e019e20e1fa18a
N-ibrahimi <nasim_ebi@yahoo.com>
2019-09-03 23:28:00 +0200

    1er projet test of order list with fonction

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -8,9 +8,9 @@ public class Order {
      */
     public void displayAvailableMenu() {
         System.out.println("liste de choix");
-        System.out.println("1- le poisson");
-        System.out.println("2- végétran");
-        System.out.println("3- la salade ");
+        System.out.println("1- poulet");
+        System.out.println("2- boeuf");
+        System.out.println("3- végétarien");
         System.out.println("choisissez votre command svp!");
 
     }
@@ -19,6 +19,14 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        System.out.println("Vous avez choisi le menu "+ nbMenu);
+        if(nbMenu==1){
+            System.out.println("Vous avez choisi comme menu : poulet");
+        } else if(nbMenu==2){
+            System.out.println("Vous avez choisi comme menu : boeuf");
+        } else if(nbMenu==3){
+            System.out.println("Vous avez choisi comme menu : végétarien");
+        } else{
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
     }
 }
